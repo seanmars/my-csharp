4570e52cc5ba0080e39be0a9f02798f8fbd28010
Mars <mars0222@gmail.com>
2018-09-11 14:48:33 +0800

    Update ApplicationDbContext.cs

--- ApplicationDbContext.cs
+++ ApplicationDbContext.cs
@@ -10,23 +10,29 @@ namespace AdminSite.Data
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
 
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        protected override void OnModelCreating(ModelBuilder builder)
         {
-            base.OnModelCreating(modelBuilder);
+            base.OnModelCreating(builder);
             // Customize the ASP.NET Identity model and override the defaults if needed.
             // For example, you can rename the ASP.NET Identity table names and more.
             // Add your customizations after calling base.OnModelCreating(builder);
 
             // Rename the Identity table names
-            modelBuilder.Entity<IdentityUser<int>>().ToTable("Users");
-            modelBuilder.Entity<IdentityRole<int>>().ToTable("Roles");
-            modelBuilder.Entity<IdentityUserClaim<int>>().ToTable("UserClaims");
-            modelBuilder.Entity<IdentityUserRole<int>>().ToTable("UserRoles");
-            modelBuilder.Entity<IdentityUserLogin<int>>().ToTable("UserLogins");
-            modelBuilder.Entity<IdentityRoleClaim<int>>().ToTable("RoleClaims");
-            modelBuilder.Entity<IdentityUserToken<int>>().ToTable("UserTokens");
-
-            CreateDefaultRoles(modelBuilder);
+            builder.Entity<IdentityUser<int>>().ToTable("Users");
+            builder.Entity<IdentityRole<int>>().ToTable("Roles");
+            builder.Entity<IdentityUserClaim<int>>().ToTable("UserClaims");
+            builder.Entity<IdentityUserRole<int>>().ToTable("UserRoles");
+            builder.Entity<IdentityUserLogin<int>>().ToTable("UserLogins");
+            builder.Entity<IdentityRoleClaim<int>>().ToTable("RoleClaims");
+            builder.Entity<IdentityUserToken<int>>().ToTable("UserTokens");
+            // or 
+            // builder.Entity<IdentityUser<int>>().ToTable("users");
+            // builder.Entity<IdentityRole<int>>().ToTable("roles");
+            // builder.Entity<IdentityUserClaim<int>>().ToTable("user_claims");
+            // builder.Entity<IdentityUserRole<int>>().ToTable("user_roles");
+            // builder.Entity<IdentityUserLogin<int>>().ToTable("user_logins");
+            // builder.Entity<IdentityRoleClaim<int>>().ToTable("role_claims");
+            // builder.Entity<IdentityUserToken<int>>().ToTable("user_tokens");
         }
     }
 }
