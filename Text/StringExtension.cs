d6d87ec2e3f96a32dfc32917452b43f6b9a6a0fd
Mars <mars0222@gmail.com>
2018-12-28 11:09:36 +0800

    Update StringExtension.cs

--- Text/StringExtension.cs
+++ Text/StringExtension.cs
@@ -1,5 +1,30 @@
 public static class StringExtension
 {
+    static readonly char[] padding = { '=' };
+
+    public static string ToUrlSafeBase64(this string raw)
+    {
+        var bytes = System.Text.Encoding.Unicode.GetBytes(raw);
+        return System.Convert.ToBase64String(bytes)
+            .TrimEnd(padding).Replace('+', '-').Replace('/', '_');
+    }
+
+    public static string FromUrlSafeBase64(this string base64)
+    {
+        var incoming = base64.Replace('_', '/').Replace('-', '+');
+        switch (base64.Length % 4)
+        {
+            case 2:
+                incoming += "==";
+                break;
+            case 3:
+                incoming += "=";
+                break;
+        }
+        var bytes = System.Convert.FromBase64String(incoming);
+
+        return System.Text.Encoding.Unicode.GetString(bytes);
+    }
     public static string ToSnakeCase(this string input)
     {
         if (string.IsNullOrEmpty(input))
