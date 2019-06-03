09d0b3639084216821827e3823acddd082e4caa2
Mars <mars0222@gmail.com>
2019-06-03 14:24:29 +0800

    Update ApplicationDbContext.cs

--- ApplicationDbContext.cs
+++ ApplicationDbContext.cs
@@ -1,30 +1,29 @@
-using AdminCore.Common;
-using AdminCore.Data;
-using AdminCore.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
-namespace AdminSite.Data
+public class ApplicationDbContext : IdentityDbContext<IdentityUser<int>, IdentityRole<int>, int>
 {
-    public class ApplicationDbContext : IApplicationDbContext
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+    protected override void OnModelCreating(ModelBuilder builder)
     {
-        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+        base.OnModelCreating(builder);
+        // Customize the ASP.NET Identity model and override the defaults if needed.
+        // For example, you can rename the ASP.NET Identity table names and more.
+        // Add your customizations after calling base.OnModelCreating(builder);
 
-        protected override void OnModelCreating(ModelBuilder builder)
-        {
-            base.OnModelCreating(builder);
-            // Customize the ASP.NET Identity model and override the defaults if needed.
-            // For example, you can rename the ASP.NET Identity table names and more.
-            // Add your customizations after calling base.OnModelCreating(builder);
+        // Rename the Identity table names
+        builder.Entity<IdentityUser<int>>().ToTable("Users");
+        builder.Entity<IdentityUserClaim<int>>().ToTable("UserClaims");
+        builder.Entity<IdentityUserLogin<int>>().ToTable("UserLogins");
+        builder.Entity<IdentityUserToken<int>>().ToTable("UserTokens");
+        builder.Entity<IdentityUserRole<int>>().ToTable("UserRoles");
 
-            // Rename the Identity table names
-            builder.Entity<IdentityUser<int>>().ToTable("Users");
-            builder.Entity<IdentityRole<int>>().ToTable("Roles");
-            builder.Entity<IdentityUserClaim<int>>().ToTable("UserClaims");
-            builder.Entity<IdentityUserRole<int>>().ToTable("UserRoles");
-            builder.Entity<IdentityUserLogin<int>>().ToTable("UserLogins");
-            builder.Entity<IdentityRoleClaim<int>>().ToTable("RoleClaims");
-            builder.Entity<IdentityUserToken<int>>().ToTable("UserTokens");
-        }
+        builder.Entity<IdentityRole<int>>().ToTable("Roles");
+        builder.Entity<IdentityRoleClaim<int>>().ToTable("RoleClaims");
     }
 }
