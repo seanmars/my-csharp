964a862ad3722366f9b2ef90c435714fc708106b
Mars <mars0222@gmail.com>
2020-02-11 15:39:59 +0800

    Update TypeNameExtension.cs

--- System/TypeNameExtension.cs
+++ System/TypeNameExtension.cs
@@ -4,12 +4,22 @@ using System.CodeDom;
 
 public static class TypeNameExtension
 {
+  /*
+    - Convert String to string and Int32 to int etc
+    - Deal with Nullable<Int32> as int? etc
+    - Suppress System.DateTime to be DateTime
+    - All other types are written in full
+  */
   public static string GetSimplifiedName(this Type t)
   {
     using (var provider = new CSharpCodeProvider())
     {
-        var typeRef = new CodeTypeReference(t);
-        return provider.GetTypeOutput(typeRef);
+      if (t.IsGenericType && t.GetGenericTypeDefinition() == typeof(Nullable<>))
+      {
+          return provider.GetTypeOutput(new CodeTypeReference(t.GetGenericArguments()[0])).Replace("System.", "") + "?";
+      }
+
+      return provider.GetTypeOutput(new CodeTypeReference(t)).Replace("System.", "");
     }    
   }
 }
