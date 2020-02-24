a752b770b3c5de6c6c2a7b41d7d8dced4b883bf9
Mars <mars0222@gmail.com>
2020-02-24 17:31:04 +0800

    Update EnumerableExtensions.cs

--- System/EnumerableExtensions.cs
+++ System/EnumerableExtensions.cs
@@ -10,4 +10,44 @@ public static class EnumerableExtensions
             source = source.Skip(chunksize);
         }
     }
+    
+    public static Dictionary<TKey, List<TValue>> GroupByDictionary<TKey, TValue>(
+            this IEnumerable<TValue> items,
+            Func<TValue, TKey> keySelector)
+    {
+        var dictionary = new Dictionary<TKey, List<TValue>>();
+        foreach (var item in items)
+        {
+            var key = keySelector(item);
+            if (!dictionary.TryGetValue(key, out var grouping))
+            {
+                grouping = new List<TValue>(1);
+                dictionary.Add(key, grouping);
+            }
+
+            grouping.Add(item);
+        }
+
+        return dictionary;
+    }
+
+    public static Dictionary<TKey, List<TValue>> GroupByDictionary<TOutput, TKey, TValue>(
+        this IEnumerable<TOutput> items,
+        Func<TOutput, TKey> keySelector, Func<TOutput, TValue> valueSelector)
+    {
+        var dictionary = new Dictionary<TKey, List<TValue>>();
+        foreach (var item in items)
+        {
+            var key = keySelector(item);
+            if (!dictionary.TryGetValue(key, out var grouping))
+            {
+                grouping = new List<TValue>(1);
+                dictionary.Add(key, grouping);
+            }
+
+            grouping.Add(valueSelector(item));
+        }
+
+        return dictionary;
+    }
 }
